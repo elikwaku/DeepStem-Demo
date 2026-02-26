9c3fe6daec76cd8cf1d2ad5f49889b0c8dcb90c5
elikwaku <elinamtamakloe98@gmail.com>
2026-02-26 13:03:32 +0000

    New

---
+++ myFirstWebsite/src/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <h1>My Heading</h1>
+        </div>
+        <ol>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ol>
+    </header>
+  );
+}
+export default Header;
